6edd380e5e2b4f8b5bca16f4aef281a1a8bf4d0d
Yann Seznec <yannseznec@gmail.com>
2023-11-23 15:24:51 +0100

    Update slideshow.py

--- slideshow.py
+++ slideshow.py
@@ -4,7 +4,7 @@ import os
 
 print("Slideshow running soon…")
 time.sleep(10)
-os.system(“feh -Y -x -q -D 7 -B black -F -Z -r /media/”)
+os.system("feh -Y -x -q -D 7 -B black -F -Z -r /media/")
 
 while 1:
   time.sleep(1)

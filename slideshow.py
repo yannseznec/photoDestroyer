3d0149463d5e12a19ba249e89931d44c4d4f9524
Yann Seznec <yannseznec@gmail.com>
2023-12-04 12:00:02 +0100

    Update slideshow.py

--- slideshow.py
+++ slideshow.py
@@ -5,10 +5,12 @@ import random
 import threading
 
 def displayDestroy():
+  os.system("killall feh")
   threading.Timer(5.0, displayDestroy).start()
   random_file=random.choice(os.listdir("photos"))
   print(random_file)
   os.system("feh -F photos/"+random_file)
+  os.system("killall feh")
 
 
 displayDestroy()

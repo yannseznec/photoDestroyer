b785adde22231e7e8f5338aa52ed67c7b1009e0c
Yann Seznec <yannseznec@gmail.com>
2023-12-04 13:47:27 +0100

    Update displayDestroy.py

--- displayDestroy.py
+++ displayDestroy.py
@@ -9,7 +9,7 @@ def displayDestroy():
   random_file="photos/"+random.choice(os.listdir("photos"))
   print(random_file)
   os.system("feh -F --reload 0.1 "+random_file)
-  # os.system("convert -resize 10% -filter Point -resize 1000% -filter Point "+random_file+" "+random_file)
+  os.system("convert -resize 10% -filter Point -resize 1000% -filter Point "+random_file+" "+random_file)
   
   
 displayDestroy()

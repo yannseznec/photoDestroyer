82f022e73c356acb54a953d15c27a91bf2eac5f9
Yann Seznec <yannseznec@gmail.com>
2023-12-04 11:43:46 +0100

    Update slideshow.py

--- slideshow.py
+++ slideshow.py
@@ -5,8 +5,9 @@ import random
 
 print("Slideshow running soon…")
 random_file=random.choice(os.listdir("photos"))
+print(random_file)
 time.sleep(1)
-os.system("feh -F "+"random_file")
+os.system("feh -F "+random_file)
 
 while 1:
   time.sleep(1)

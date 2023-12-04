d84bd3926f609a76f84d3598467485a023b921a3
Yann Seznec <yannseznec@gmail.com>
2023-12-04 13:14:10 +0100

    Update displayDestroy.py

--- displayDestroy.py
+++ displayDestroy.py
@@ -5,10 +5,11 @@ import random
 import threading
 
 def displayDestroy():
-  threading.Timer(5.0, displayDestroy).start()
+  # threading.Timer(5.0, displayDestroy).start()
   random_file="photos/"+random.choice(os.listdir("photos"))
   print(random_file)
   os.system("feh -F "+random_file)
+  time.sleep(3)
   os.system("convert -resize 10% -filter Point -resize 1000% -filter Point "+random_file+" "+random_file)
   os.system("killall feh")
   

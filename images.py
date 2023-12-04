1d1350f34bfd276ddf8f37db05261c20ba65e4cb
Yann Seznec <yannseznec@gmail.com>
2023-12-04 11:23:07 +0100

    fix

--- images.py
+++ images.py
@@ -1,1 +1,3 @@
+import os
+
 os.system("feh -qrzZFD 3.0 photos")
--- simpleImage.py
+++
@@ -1,7 +0,0 @@
-# importing Image class from PIL package 
-from PIL import Image 
-
-# creating a object 
-im = Image.open(r"/Users/yann/Desktop/photoDestroyer/test.jpeg") 
-
-im.show()

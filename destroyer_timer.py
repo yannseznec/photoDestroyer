44e22305658797a510daac0a60304074207ca7cc
Yann Seznec <yannseznec@gmail.com>
2023-12-06 10:09:07 +0100

    no more usb stick for now :(

--- destroyer_timer.py
+++ destroyer_timer.py
@@ -18,7 +18,7 @@ def timeCounter():
   currentTime+=3
   resize1 = str((1/currentTime)*100)
   resize2 = str(currentTime*100)
-  os.system("feh -F --reload=3 "+random_file + "&")
+  os.system("feh -F --hidepointer --reload=3 "+random_file + "&")
   os.system("convert -resize "+resize1+"% -resize "+resize2+"% "+random_file+" "+random_file)
   event.wait(4)
   print(resize1)
@@ -30,7 +30,9 @@ def timeCounter():
 
 def randomImage():
   global random_file
-  random_file="../usb/photos/"+random.choice(os.listdir("../usb/photos"))
+  # random_file="../usb/photos/"+random.choice(os.listdir("../usb/photos"))
+  # running from a USB stick seems to make it freeze, maybe need a better stick?
+  random_file="photos/"+random.choice(os.listdir("photos"))
 
 
 

a8e18fe195978ac3f7de43d35bc2bf373c216b20
Yann Seznec <yannseznec@gmail.com>
2023-12-06 10:34:05 +0100

    whoops

--- destroyer_timer.py
+++ destroyer_timer.py
@@ -6,7 +6,7 @@ import threading
 
 currentTime = 0
 rate = 5
-displayTime = 100
+displayTime = 3
 random_file = "imagename"
 
 def timeCounter():
@@ -18,24 +18,28 @@ def timeCounter():
   currentTime+=3
   resize1 = str((1/currentTime)*100)
   resize2 = str(currentTime*100)
-  os.system("feh -F --hide-pointer --reload=3 "+random_file + "&")
   os.system("convert -resize "+resize1+"% -resize "+resize2+"% "+random_file+" "+random_file)
   event.wait(4)
   print(resize1)
   if currentTime > displayTime:
     os.system("rm "+random_file)
-    randomImage()
-    currentTime = 0
     print("deleted")
+    randomImage()
+    os.system("feh --reload=3 "+random_file + "&")
+    currentTime = 0
+    
+
 
 def randomImage():
   global random_file
   # random_file="../usb/photos/"+random.choice(os.listdir("../usb/photos"))
   # running from a USB stick seems to make it freeze, maybe need a better stick?
   random_file="photos/"+random.choice(os.listdir("photos"))
+  print(random_file)
 
 
 
 
 randomImage()
+os.system("feh --reload=3 "+random_file + "&")
 timeCounter()
